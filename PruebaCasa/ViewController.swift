6ab5132277cf695594ddcca102e1667b4468a773
Erwin Bermudez Casillas <erwin.casillas@unach.mx>
2023-05-31 22:02:12 -0600

    Initial Commit

---
+++ PruebaCasa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PruebaCasa
+//
+//  Created by Erwin Bermudez Casillas on 31/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
